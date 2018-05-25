85a53cc640eeabeadadeb9daa57821252d947464
Nelson <modafokerz@gmail.com>
2018-05-26 00:56:40 +0200

    MULTI THREAD ENFIN CA MARCHE LOL !

--- src/components/WeatherJPanel.java
+++ src/components/WeatherJPanel.java
@@ -1,7 +1,7 @@
 /*
 * Exercice FIG HES-SO (Sierre)
 * Auteur : Nelson Ribeiro Teixeira
-* Date de création : 17 mai 2018
+* Date de création : 25 mai 2018
 * Date de modification : /
 */
 package components;
@@ -12,20 +12,17 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class BackgroundPanelApp extends BackgroundPanel {
-	
-	
+public class WeatherJPanel extends JPanel {
 	public void paintComponent(Graphics g) {
-		
 		try {
-			Image img = ImageIO.read(new File("img/HomeBackground2.jpg"));
+			Image img = ImageIO.read(new File("img/weatherApp/weatherAppBackground.png"));
 			g.drawImage(img, 0, 0, this);
 			g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 }
--- src/nav/WeatherApp.java
+++ src/nav/WeatherApp.java
@@ -8,28 +8,46 @@ package nav;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingWorker;
+import javax.swing.border.LineBorder;
+
+import components.WeatherJPanel;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 
 
 
+@SuppressWarnings("serial")
 public class WeatherApp extends AppBaseFrame {
 	
 	
 	private String ville = "Sierre";
 	private String villeStatus = "Ensoleillé";
 	private String villeTemp = "25";
-	private JPanel weatherAppPanel = new JPanel();
+	private WeatherJPanel weatherAppPanel = new WeatherJPanel();
 	
+	private String apiKey = "0761f6abebe05ed6cd5e1985cdd37791";
+	private double[] cityCoordinates = new double[2];
+	
+	private String forecastUrl = "https://api.darksky.net/forecast/"+ apiKey + "/" + cityCoordinates[0]+ "," + cityCoordinates[1];
 	
 	private JPanel topAppPanel = new JPanel();
-	private JButton cityChoiceButton = new JButton();
+	private JButton cityChoiceButton = new JButton("Choisir une autre ville");
 	private JLabel cityLabel = new JLabel(ville);
 	private JLabel cityStatus = new JLabel(villeStatus);
 	private JLabel cityTemp = new JLabel(villeTemp + " °C");
@@ -40,15 +58,89 @@ public class WeatherApp extends AppBaseFrame {
 	
 	public WeatherApp() {
 		super();
-
+		httpRequest();
+		
 		remove(centerPanel);
 		add(weatherAppPanel, BorderLayout.CENTER);
 		weatherAppPanel.setSize(600, 650);
 		
-		
+		weatherAppPanel.setLayout(new FlowLayout());
+		weatherAppPanel.add(topAppPanel);
 		// Top App Weather Panel construction !
+		topAppPanel.setPreferredSize(new Dimension(600, 400));
+		topAppPanel.setOpaque(false);
+		topAppPanel.setLayout(new FlowLayout());
 		
+		// Top App Panel - Components construction
+		cityChoiceButton.setPreferredSize(new Dimension(575,30));
+		cityChoiceButton.setForeground(Color.white);
+		cityChoiceButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		cityChoiceButton.setHorizontalAlignment(SwingConstants.CENTER);
+		cityChoiceButton.setContentAreaFilled(false);
+		cityChoiceButton.setOpaque(false);
+		cityChoiceButton.setBorder(new LineBorder(Color.WHITE, 3));
 		
+		topAppPanel.add(cityChoiceButton);
+		
+		cityLabel.setPreferredSize(new Dimension(600,100));
+		cityLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		cityLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+		cityLabel.setFont(new Font("Impact", Font.PLAIN, 80));
+		cityLabel.setForeground(Color.white);
+		topAppPanel.add(cityLabel);
+		
+		cityStatus.setPreferredSize(new Dimension(600,20));
+		cityStatus.setHorizontalAlignment(SwingConstants.CENTER);
+		cityStatus.setForeground(Color.white);
+		topAppPanel.add(cityStatus);
+		
+		cityTemp.setPreferredSize(new Dimension(600,200));
+		topAppPanel.add(cityTemp);
+		
+		weatherAppPanel.add(topAppPanel, BorderLayout.CENTER);
+		
+		// Bottom App Panel construction !
+		bottomAppPanel.setPreferredSize(new Dimension(600, 250));
+		
+		weatherAppPanel.add(bottomAppPanel,BorderLayout.SOUTH);
+	}
+	
+	private void httpRequest() {
+		cityCoordinates[0] = 37.8267;
+		cityCoordinates[1] = -122.4233;
+		
+		System.out.println("blabla 1");
+		// Tâche effectuée sur un thread différent de manière à n'avoir aucune latence
+		// Youhou,,,, ca marche enfin !! putin je suis trop content mdr
+		new SwingWorker <String, Void>() {
+
+			@Override
+			protected String doInBackground() throws Exception {
+				OkHttpClient client = new OkHttpClient();
+				Request request = new Request.Builder().url(forecastUrl).build();
+				
+				Call call = client.newCall(request);
+				try {
+					Response response = call.execute();
+					return response.body().string();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				return null;
+			}
+			
+			@Override
+			protected void done () {
+				try {
+					System.out.println(get());
+				} catch (InterruptedException | ExecutionException e) {
+					e.printStackTrace();
+				} 
+			}
+			
+		}.execute();
+		System.out.println("blabla 2");
 	}
 	
 	
